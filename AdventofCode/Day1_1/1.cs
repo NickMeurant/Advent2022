c7d039cbb2609935b250d5986ab1130c5a64f619
NickMeurant <n10485571@qut.edu.au>
2022-12-06 12:42:02 +1000

    Day1 Done

---
+++ AdventofCode/Day1_1/1.cs
@@ -0,0 +1,26 @@
+﻿var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+List<string> text = File.ReadAllLines(path).ToList();
+
+List<long> elveCalories = new List<long>();
+
+long current = 0;
+long most = 0;
+
+using (StreamReader reader = new StreamReader(path))
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            elveCalories.Add(current);
+            current = 0;
+        }
+        else
+        {
+            current += long.Parse(line);
+        }
+    }
+}
+
+Console.WriteLine(elveCalories.MaxBy(x => x));
