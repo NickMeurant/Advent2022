205e57e0231be02af23842e9b397dec7ffa4337f
NickMeurant <n10485571@qut.edu.au>
2022-12-06 11:23:29 +1000

    initial commit

---
+++ AdventMain/AdventMain/Day1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventMain
+{
+    internal class Day1
+    {
+        public static void solution1()
+        {
+            string text = File.ReadAllText(Program.inputLocation + "1.txt");
+
+            List<string> caloies = text.Split("\n\n").ToList();
+            foreach (string word in caloies) {
+                Console.WriteLine(word);
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
---
+++ AdventMain/AdventMain/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventMain // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static string inputLocation = "C:\\Users\\Nicho\\OneDrive\\Documents\\Advent2022\\AdventMain\\AdventMain\\inputFiles\\";
+        static void Main(string[] args)
+        {
+            Day1.solution1();
+        }
+    }
+}
