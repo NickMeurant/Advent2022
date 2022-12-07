679c3c66e16e70fd424bb3c2b8a5f852e4a2cd34
NickMeurant <n10485571@qut.edu.au>
2022-12-07 11:34:49 +1000

    2_1 finished

---
+++ AdventofCode/Day2_1/2.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+long totalScore = 0;
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+
+List<string> text = File.ReadAllLines(path).ToList();
+
+using (StreamReader reader = new StreamReader(path))
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        var parts = line.Split(" ");
+        var opponentShape = (Shape)(parts[0][0] - 'A');
+        var ourShape = (Shape)(parts[1][0] - 'X');
+        totalScore += CalculateRoundScore(opponentShape, ourShape);
+    }
+}
+
+long CalculateRoundScore(Shape opponent, Shape we)
+{
+    var shapeScore = GetShapeScore(we);
+    var roundScore = 3;
+    if (opponent != we)
+    {
+        int ourShapeIndex = (int)we;
+        int opponentShapeIndex = (int)opponent;
+        roundScore = ourShapeIndex == (opponentShapeIndex + 1) % 3 ? 6 : 0;
+    }
+
+    return shapeScore + roundScore;
+}
+
+Console.WriteLine(totalScore.ToString()); // answer
+
+long GetShapeScore(Shape shape) => (int)shape + 1;
+
+enum Shape
+{
+    Rock,
+    Paper,
+    Scissors
+}
