83770e0b89528d96b479212b87019b01e7792f68
NickMeurant <n10485571@qut.edu.au>
2022-12-07 16:03:26 +1000

    day5_1 finished

--- AdventofCode/Day5_1/1.cs
+++ AdventofCode/Day5_1/1.cs
@@ -4,10 +4,59 @@ var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
 
 var lines = File.ReadAllLines(path);
 
-foreach (string line in lines)
+var blankLine = Array.IndexOf(lines, string.Empty); // diagram ends when all blank lines
+
+var numberOfStacks = int.Parse(lines[blankLine - 1]
+    .Trim()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Last());
+
+var stacks = new Stack<char>[numberOfStacks];
+
+// get stack info from diagram
+
+for(int line = blankLine - 2; line >= 0; line--)
 {
-    Console.WriteLine(line);
+    string crateLine = lines[line];
+    Console.WriteLine(crateLine);
+
+    for(int i = 0; i < numberOfStacks; i++)
+    {
+        char crate = crateLine[i*4 + 1];
+        if (Char.IsLetter(crate))
+        {
+            stacks[i] ??= new Stack<char>();
+            stacks[i].Push(crate);
+        }
+    }
 }
-Console.ReadLine();
+
+// read instructions, instructions start on blankLine + 1 (10)
+
+for (int lineNumber = blankLine + 1; lineNumber < lines.Length; lineNumber++)
+{
+    string commands = lines[lineNumber];
+
+    Console.WriteLine(commands);
+
+    int amount = int.Parse(commands.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+
+    int fromStack = int.Parse(commands.Split(" ", StringSplitOptions.RemoveEmptyEntries)[3]) -1;
+
+    int toStack = int.Parse(commands.Split(" ", StringSplitOptions.RemoveEmptyEntries)[5]) -1;
+
+    for (int i = 0; i< amount; i++)
+    {
+        char crate = stacks[fromStack].Pop();
+        stacks[toStack].Push(crate);
+    }
+}
+
+for(int i = 0;i<stacks.Length; i++)
+{
+    Console.Write(stacks[i].Peek().ToString());
+}
+
+
 
 
