6a9b1b41367ebdf8bac44042bb04998648882e75
NickMeurant <n10485571@qut.edu.au>
2022-12-07 16:14:16 +1000

    Day5_2 done

---
+++ AdventofCode/Day5_2/2.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+
+var lines = File.ReadAllLines(path);
+
+var blankLine = Array.IndexOf(lines, string.Empty); // diagram ends when all blank lines
+
+var numberOfStacks = int.Parse(lines[blankLine - 1]
+    .Trim()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Last());
+
+var stacks = new Stack<char>[numberOfStacks];
+
+// get stack info from diagram
+
+for(int line = blankLine - 2; line >= 0; line--)
+{
+    string crateLine = lines[line];
+    Console.WriteLine(crateLine);
+
+    for(int i = 0; i < numberOfStacks; i++)
+    {
+        char crate = crateLine[i*4 + 1];
+        if (Char.IsLetter(crate))
+        {
+            stacks[i] ??= new Stack<char>();
+            stacks[i].Push(crate);
+        }
+    }
+}
+
+// read instructions, instructions start on blankLine + 1 (10)
+
+for (int lineNumber = blankLine + 1; lineNumber < lines.Length; lineNumber++)
+{
+    string commands = lines[lineNumber];
+
+    int amount = int.Parse(commands.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+
+    int fromStack = int.Parse(commands.Split(" ", StringSplitOptions.RemoveEmptyEntries)[3]) -1;
+
+    int toStack = int.Parse(commands.Split(" ", StringSplitOptions.RemoveEmptyEntries)[5]) -1;
+
+    Stack<char> tempStack = new Stack<char>();
+
+    for (int i = 0; i< amount; i++)
+    {
+        char crate = stacks[fromStack].Pop();
+        tempStack.Push(crate);
+    }
+    for(int i = 0; i < amount; i++)
+    {
+        char crate = tempStack.Pop();
+        stacks[toStack].Push(crate);
+    }
+}
+
+for(int i = 0;i<stacks.Length; i++)
+{
+    Console.Write(stacks[i].Peek().ToString());
+}
+
+
+
+
