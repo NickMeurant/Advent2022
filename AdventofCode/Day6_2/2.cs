5789840e804f8be47913d594cecfd48d88396d65
NickMeurant <n10485571@qut.edu.au>
2022-12-07 17:51:54 +1000

    Day6_2 Done

---
+++ AdventofCode/Day6_2/2.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Net;
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+
+var lines = File.ReadAllLines(path);
+
+string line = lines[0];
+int left = 0;
+
+int NEEDEDLENGTH = 14;
+
+while (left + NEEDEDLENGTH < line.Length - 1)
+{
+    string currentSubString = line.Substring(left, NEEDEDLENGTH);
+
+    if (new HashSet<char>(currentSubString.ToCharArray()).Count == NEEDEDLENGTH)
+    {
+        Console.WriteLine(left + NEEDEDLENGTH);
+        return;
+    }
+    else
+    {
+        left++;
+    }
+}
