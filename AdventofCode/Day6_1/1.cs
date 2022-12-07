93ea91e5f1e9702b4cc7d30692ac8ea7adc8a59d
NickMeurant <n10485571@qut.edu.au>
2022-12-07 17:17:49 +1000

    AHHHHHHHHHHHHH

---
+++ AdventofCode/Day6_1/1.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Net;
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+
+var lines = File.ReadAllLines(path);
+
+string line = lines[0];
+int left = 0;
+int right = 1;
+
+HashSet<char> found = new HashSet<char>();
+
+while (right < line.Length - 1)
+{
+    if (right - left < 3)
+    {
+        found.Add(line[right]);
+        found.Add(line[left]);
+        right++;
+        continue;
+    }
+
+    found.Remove(line[left]);
+    left++;
+
+    right++;
+    found.Add(line[right]);
+
+    if (found.Count == 4)
+    {
+        Console.WriteLine(right - 1);
+        break;
+    }
+}
